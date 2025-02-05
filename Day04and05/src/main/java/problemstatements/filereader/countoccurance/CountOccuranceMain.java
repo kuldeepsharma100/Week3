0d0202a30a6120231f2fd826382400995efe566b
Kuldeep <sharmakuldeep74420@gmail.com>
2025-02-05 18:42:56 +0530

    [Kuldeep Sharma] add. Count occurrence of the word in a file

---
+++ Day04and05/src/main/java/problemstatements/filereader/countoccurance/CountOccuranceMain.java
@@ -0,0 +1,7 @@
+package problemstatements.filereader.countoccurance;
+
+public class CountOccuranceMain {
+    public static void main(String arg[]){
+        CountOccurance.countOccurance();
+    }
+}
