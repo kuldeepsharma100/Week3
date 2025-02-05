526701ebfbe100b4f767f0a8fb0930ff13f3d35e
Kuldeep <sharmakuldeep74420@gmail.com>
2025-02-05 18:43:23 +0530

    [Kuldeep Sharma] add.

---
+++ Day04and05/src/main/java/problemstatements/filereader/readlinebyline/ReadFileMain.java
@@ -0,0 +1,9 @@
+package problemstatements.filereader.readlinebyline;
+
+import problemstatements.filereader.readlinebyline.ReadFile;
+
+public class ReadFileMain {
+    public static void main(String [] args){
+        ReadFile.readFile();
+    }
+}
