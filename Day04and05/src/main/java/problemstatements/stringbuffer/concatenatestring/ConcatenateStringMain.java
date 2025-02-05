4080f89fb3031b7579e5d9f5b77e4e4099314739
Kuldeep <sharmakuldeep74420@gmail.com>
2025-02-05 19:15:15 +0530

    [Kuldeep Sharma] add. Add method to concatenate strings using StringBuffer

---
+++ Day04and05/src/main/java/problemstatements/stringbuffer/concatenatestring/ConcatenateStringMain.java
@@ -0,0 +1,9 @@
+package problemstatements.stringbuffer.concatenatestring;
+
+public class ConcatenateStringMain {
+    public static void main(String[] args) {
+        String[] input = {"Hello", " ", "World"};
+        String result = CancatenateString.concatenateStrings(input);
+        System.out.println("Concatenated String: " + result);
+    }
+}
